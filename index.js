d5338f3d2d2994c5b3e62c658396d8cb63ce5f3d
Abhibagad <118585905+Abhibagad@users.noreply.github.com>
2023-03-28 21:33:50 -0700

    function

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// function Abc(){
+//     return a;
+//     let a=2;
+// }
+// Abc();
+
+// let a=3;
+// let a=4;
+// function sum(){
+//     return a+b;
+// }
+// sum();
+
+// const Abc = () =>  20+30
+// console.log(Abc());
+
+
+let n=5;
+function fac(){
+    for(let i=1; i<=n;i++){
+        let fc=n*(n-1);
+        
+    }
+    return fc;
+}
+fac()
+
